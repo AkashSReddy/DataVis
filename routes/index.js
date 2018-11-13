b4670d1138eef7352d41aec92226db23846193b4
AkashSReddy <akashsreddy98@gmail.com>
2018-11-13 23:08:05 +0530

    Basic Graphs Added

--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,54 @@
 var express = require("express");
 var router = express.Router();
 var mysql = require("mysql");
-
 var con = mysql.createConnection({
   host: "localhost",
   user: "akash",
   password: "password",
   database: "visual"
 });
+var regno = 0;
 
 con.connect(err => {
   if (err) throw err;
   console.log("Connected");
 });
 
-/* GET users listing. */
-// con.query("desc CSE2001_C", (err, result, fields) => {
-//   if (err) throw err;
-// });
-
 router.get("/", (req, res, next) => {
   res.send("HEY");
 });
 router.get("/login", (req, res, next) => {
-  console.log("Yo");
   res.render("login", { title: "DBMS" });
 });
 
 router.post("/", (req, res, next) => {
-  console.log(req.body);
+  regno = req.body.regno;
   res.redirect("/getclass");
 });
+
 router.get("/getclass", (req, res, next) => {
-  con.query("show tables", (err, results, fields) => {
+  var queryy = "select course_code from " + regno;
+  // var queryy = "desc 17bci0097";
+
+  console.log(queryy);
+  con.query(queryy, (err, results, fields) => {
     if (err) throw err;
     else {
-      res.json(results);
+      res.render("class", { title: " DBMS", data: results });
     }
   });
 });
 
-router.get("/getmarks", (req, res, next) => {});
-
+router.get("/:id", (req, res, next) => {
+  var idd = req.path;
+  idd = idd.substr(1);
+  console.log(idd);
+  // console.log(queryy);
+  // con.query(queryy, (err, results, fields) => {
+  //   if (err) throw err;
+  //   else {
+  //     res.render("class", { title: " DBMS", data: results });
+  //   }
+  res.render("marks", { title: "DBMS" });
+});
 module.exports = router;
